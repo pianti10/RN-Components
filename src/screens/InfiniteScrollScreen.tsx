6fddca76b9bddd8fad04f16b7928cc5bdc0707b3
pianti10 <nicolaspiantini@gmail.com>
2023-07-08 19:30:17 -0300

    Infinite scroll con imagenes

--- src/screens/InfiniteScrollScreen.tsx
+++ src/screens/InfiniteScrollScreen.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  FlatList,
+  ActivityIndicator,
+} from 'react-native';
 import {HeaderTitle} from '../components/HeaderTitle';
-import {useState} from 'react';
-import {FlatList} from 'react-native-gesture-handler';
 
 export const InfiniteScrollScreen = () => {
   const [numbers, setNumbers] = useState([0, 1, 2, 3, 4, 5]);
@@ -12,11 +17,21 @@ export const InfiniteScrollScreen = () => {
     for (let i = 0; i < 5; i++) {
       newArray[i] = numbers.length + i;
     }
-    setNumbers([...numbers, ...newArray]);
+    setTimeout(() => {
+      setNumbers([...numbers, ...newArray]);
+    }, 1500);
   };
 
   const renderItem = (item: number) => {
-    return <Text style={styles.textItem}>{item}</Text>;
+    return (
+      <Image
+        source={{uri: `https://picsum.photos/id/${item}/500/400`}}
+        style={{
+          width: '100%',
+          height: 400,
+        }}
+      />
+    );
   };
 
   return (
@@ -28,6 +43,17 @@ export const InfiniteScrollScreen = () => {
         ListHeaderComponent={<HeaderTitle title="Infinite Scroll" />}
         onEndReached={loadMore}
         onEndReachedThreshold={0.5}
+        ListFooterComponent={() => (
+          <View
+            style={{
+              height: 150,
+              width: '100%',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <ActivityIndicator size={25} color="#5856D6" />
+          </View>
+        )}
       />
     </View>
   );
